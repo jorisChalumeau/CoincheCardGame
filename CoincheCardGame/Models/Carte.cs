91b817ea80e65038129a92bb9977710fc8328518
TheApac <apac.alexcuevas@gmail.com>
2018-05-25 10:57:16 +0200

    Added Cards and Deck
    Ajout enum couleur
    Ajout classes carte et deck
    Ajout fonction shuffle de deck

---
+++ CoincheCardGame/Models/Carte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CoincheCardGame.Models
+{
+    public enum Couleur
+    {
+        Coeur,
+        Pique,
+        Carreau,
+        Trefle
+    };
+    
+    public class Carte
+    {
+        private Couleur _couleur;
+        private String valeur;
+
+        public Carte(Couleur c, String val)
+        {
+            _couleur = c;
+            valeur = val;
+        }
+    }
+}
---
+++ CoincheCardGame/Models/Deck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoincheCardGame.Models
+{
+    public class Deck
+    {
+        public List<Carte> Cartes;
+        public Deck()
+        {
+            Cartes = new List<Carte>();
+            foreach (Couleur c in Enum.GetValues(typeof(Couleur)))
+            {
+                for (int i = 7; i < 11; i++)
+                {
+                    Cartes.Add(new Carte(c, i.ToString()));
+                }
+                Cartes.Add(new Carte(c, "J"));
+                Cartes.Add(new Carte(c, "Q"));
+                Cartes.Add(new Carte(c, "R"));
+                Cartes.Add(new Carte(c, "A"));
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random random = new Random();
+            for (int i = Cartes.Count; i > 1; i--)
+            {
+                int rand = random.Next(i + 1);
+                Carte save = Cartes[rand];
+                Cartes[rand] = Cartes[i];
+                Cartes[i] = save;
+            }
+
+            int nb = random.Next(4, 30);
+            List<Carte> cartes2 = new List<Carte>();
+            for (int i = nb; i < Cartes.Count; i++)
+            {
+                cartes2.Add(Cartes[i]);
+            }
+            for (int i = 0; i < nb; i++)
+            {
+                cartes2.Add(Cartes[i]);
+            }
+            for (int i = 0; i < Cartes.Count; i++)
+            {
+                Cartes[i] = cartes2[i];
+            }
+        }
+    }
+}
